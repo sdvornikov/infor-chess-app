16d69730049ca0d35c6046f87f133731261b6bde
Sergey <sergey.dvornikov@gmail.com>
2017-07-30 16:53:28 -0400

    navigation routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from '@angular/common'
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PlayerListComponent } from './player-list/player-list.component';
 
 import { inforChessInjectables } from './app.injecrables';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'players', pathMatch: 'full' },
+  { path: 'players', component: PlayerListComponent },
+  { path: 'about', component: AboutComponent },
+
+  // { path: 'player/:id', component: PlayerDetailsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    PlayerListComponent
+    PlayerListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [inforChessInjectables],
   bootstrap: [AppComponent]
